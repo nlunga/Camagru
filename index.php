b0dc3be18d0acafa791487bb3201585d39651324
Nqabutho LUNGA <nlunga@c5r7s7.wethinkcode.co.za>
2019-10-21 17:08:55 +0200

    fix the login

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Camagru</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+  </head>
+  <body>
+    <center>
+      <div class="form-container">
+        <form class="myForm" action="index.html" method="post">
+          <input type="email" name="user-email" value="" placeholder="Email"><br>
+          <input type="text" name="fullname" value="" placeholder="Full Name"><br>
+          <input type="text" name="username" value="" placeholder="Username"><br>
+          <input type="password" name="passwd" value="" placeholder="Password"><br>
+          <input type="submit" id="sign_up" name="submit" value="Sign up">
+        </form>
+        <p class="sign">By signing up, you agree to our <br><strong>Terms</strong> , <strong>Data Policy</strong> and <strong>Cookies</strong> <br>Policy .</p>
+      </div>
+      <div class="log">
+        <p>Have an account? <a href="login.php">Log in</a></p>
+      </div>
+    </center>
+  </body>
+</html>
