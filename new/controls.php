543d8cda8d11b65624b3f17576b193d64ecab0a8
Innocent <nlunga@student.wethinkcode.co.za>
2019-11-04 21:47:06 +0200

    progress made

--- new/controls.php
+++ new/controls.php
@@ -1,6 +1,5 @@
 <?php
   //session_start();
-  //require 'connect.php';
   include 'create.php';
 
   $UserNameError = "";
@@ -21,25 +20,21 @@
 
   if (isset($_POST['submit'])) {
     if (empty($username)) {
-      //$UserNameError = "Please enter username";
       $error['UserNameError'] = "Please enter a username";
     }else {
       $username = test_user_input($username);
     }
     if (empty($userEmail)) {
-      //$EmailError = "Please enter an email address";
       $error['EmailError'] = "Please enter an email address";
     }else {
       $userEmail = test_user_input($userEmail);
     }
     if (empty($password)) {
-      //$PasswordError = "Please enter a password";
       $error['PasswordError'] = "Please enter a password";
     }else {
       $password = test_user_input($password);
     }
     if ($confPass !== $password) {
-      //$ConfPasswordError = "Invalid password";
       $error['ConfPasswordError'] = "Password does no match";
     }
     /* //uncomment
