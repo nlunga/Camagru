a052fef63af61bc5b19925e6a29cef322a7f6696
Nqabutho Lunga <nlunga@student.wethinkcode.co.za>
2019-12-05 12:28:09 +0200

    this is working

--- index.php
+++ index.php
@@ -1,12 +1,54 @@
+<?php
+  require_once 'controls.php';
+
+  if (isset($_GET['token'])) {
+    $token = $_GET['token'];
+    verifyUser($token);
+  }
+
+  if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+  }else {
+    $page = 1;
+  }
+
+  if (isset($_GET['password-token'])) {
+    $passwordToken = $_GET['password-token'];
+    resetPassword($passwordToken);
+  }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
-    <title>Camagru</title>
+    <meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Camagru</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="css/style.css" media="all" />
   </head>
   <body>
-    <h2>Camagru</h2>
-    <p>You are currently not signed in <a href="login.php">Login</a> Not yet a member? <a href="signup.php">Sign up</a></p>
-    <p>You are logged in as {username} <a href="new/logout.php">Log out</a> </p>
+    <div id="header">
+      <div id="logo">
+        <h1>Camagru</h1>
+      </div>
+      <div id="nav">
+        <?php if (!isset($_SESSION['verified'])): ?>
+          <a class="tab" href="signup.php">Sign Up</a>
+          <a class="tab" href="login.php">Log in</a>
+        <?php endif; ?>
+        <?php if (isset($_SESSION['verified'])): ?>
+          <a class="tab" href="profile.php">My Profile</a>
+          <a class="tab" href="index.php?logout=1">Log out</a>
+        <?php endif; ?>
+      </div>
+    </div>
+    <div id="container">
+      <?php
+        require_once 'imageInsert.php';
+        publicImage("images", $page);
+      ?>
+    </div>
   </body>
 </html>
